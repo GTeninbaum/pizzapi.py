33d3a6c4844c8b7ba0f544745b92886cf00470e3
Aaron Thomas <aaronthomas.handyman@gmail.com>
2016-09-16 12:59:59 -0500

    UI and math improvements
    Uses pow() and math.pi for precision

--- v3.py
+++ v3.py
@@ -1,15 +1,17 @@
-print("\n\nWELCOME TO PIZZA PI R SQUARED. \n\nWhat's it do?  \n\nIt lets you determine whether buying a small pizza or a large pizza is a better value.")
+import math
+
+print("WELCOME TO PIZZA PI R SQUARED. \n\nWhat's it do?  \nIt lets you determine whether buying a small pizza or a large pizza is a better value.")
  
-diameter_one = float(input("\nWhat's the first pizza's diameter (in inches)?"))
-cost_one = float(input("\nHow much does the first pizza cost? (in dollars and cents)"))
-area_one = (diameter_one / 2) **2 * 3.14
+diameter_one = float(input("\nWhat's the first pizza's diameter (in inches)? "))
+cost_one = float(input("How much does the first pizza cost? (in dollars and cents) "))
+area_one = pow((diameter_one / 2), 2) * math.pi
 total_one = round(cost_one / area_one, 2)
 
 print("\nWith this pizza, you're paying $%s per square inch." % (total_one))
 
-diameter_two = float(input("\nWhat's the second pizza's diameter (in inches)?"))
-cost_two = float(input("\nHow much does the second pizza cost? (in dollars and cents)"))
-area_two = (diameter_two / 2) **2 * 3.14
+diameter_two = float(input("\nWhat's the second pizza's diameter (in inches)? "))
+cost_two = float(input("How much does the second pizza cost? (in dollars and cents) "))
+area_two = pow((diameter_two / 2), 2) * math.pi
 total_two = round(cost_two / area_two, 2)
 
 print("\nWith this pizza, you're paying $%s per square inch." % (total_two))
